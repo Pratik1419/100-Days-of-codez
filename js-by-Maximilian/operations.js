8b201b7114f5a9b286c842d624ce55991790c0d9
Pratik Mishra <pratikmishra141@gmail.com>
2024-04-22 21:13:04 +0530

    Update operations.js

--- js-by-Maximilian/operations.js
+++ js-by-Maximilian/operations.js
@@ -1,3 +1,6 @@
-let userName = "Pratik"
+let userName = "Pratik";
 console.log(userName.length);
 console.log(userName.toUpperCase());
+
+let hobbies = ["Sports", "Cooling"];
+console.log(hobbies.length);

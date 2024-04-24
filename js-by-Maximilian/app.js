eb6ba1c13fe53bc5b69b3a5a21f4c5e2708bcc44
Pratik Mishra <pratikmishra141@gmail.com>
2024-04-24 21:19:35 +0530

    Update app.js

--- js-by-Maximilian/app.js
+++ js-by-Maximilian/app.js
@@ -32,8 +32,7 @@
 
 // person.greet();
 
-
-// 1. Select the <h1> element by "drilling into the DOM" and 
+// 1. Select the <h1> element by "drilling into the DOM" and
 //    save it in a variable with a name of your choice
 // answers:-
 let heading = document.body.children[0];
@@ -42,7 +41,7 @@ console.dir(heading);
 // 2. Use the variable from (1) and get access to the "parent"
 //    element of the stored <h1> element (i.e. to the <body> element),
 console.dir(heading.parentElement); // body
-//    BONUS: Try using the variable from (1) to get access to the 
+//    BONUS: Try using the variable from (1) to get access to the
 //    sibling element (i.e. the <p> element next to the <h1> element)
 console.dir(heading.nextElementSibling); // p
 
@@ -51,7 +50,7 @@ console.dir(heading.nextElementSibling); // p
 let headings = document.getElementById("headings");
 
 // 4. Select the second <p> element with querySelector (you might
-//    need to add something in the HTML code, e.g. a class) 
+//    need to add something in the HTML code, e.g. a class)
 //    and store it in a new variable with a name of your choice
 let paragraph = document.querySelector(".para");
 console.dir(paragraph);
@@ -59,3 +58,12 @@ console.dir(paragraph);
 // 5. BONUS TASK: Try changing the text content of the <p> element
 //    you selected in (4) and set it to any other text of your choice
 paragraph.textContent = "This is some random text";
+
+// Create the new element.
+let newElement = document.createElement("a");
+newElement.href = "https://facebook.com";
+newElement.textContent = "Dive into facebook";
+let para = document.querySelector("p");
+
+para.append(newElement);
+console.dir("newElement");

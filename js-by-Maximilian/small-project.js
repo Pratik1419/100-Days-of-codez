e4e673551e3de6200def6374f93dfa6a28bcc361
Pratik Mishra <pratikmishra141@gmail.com>
2024-04-25 16:39:41 +0530

    Update small-project.js

--- js-by-Maximilian/small-project.js
+++ js-by-Maximilian/small-project.js
@@ -1,15 +1,17 @@
 // let paraElement = document.querySelector("p");
-let textElement = document.querySelector("input");
+// let textElement = document.querySelector("input");
 
 // function changePara() {
 //   paraElement.textContent = "clicked!";
 //   console.log("Event Fired");
 // }
 
-function countText() {
-    let enterText = textElement.value;
-    console.log(enterText);
-  }
+// function countText(event) {
+//     let enterText = textElement.value;
+// let enterText = event.traget.value;
+// let enterText = event.data: => this is different from other two above;
+//     console.log(enterText);
+//   }
 
 // paraElement.addEventListener("click", changePara);
-textElement.addEventListener("input", countText);
+// textElement.addEventListener("input", countText);

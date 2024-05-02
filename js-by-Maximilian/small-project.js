0092b0e826c7aacf4eb2eee35102afb65be3f839
Pratik Mishra <pratikmishra141@gmail.com>
2024-05-02 20:50:48 +0530

    Update small-project.js

--- js-by-Maximilian/small-project.js
+++ js-by-Maximilian/small-project.js
@@ -1,3 +1,9 @@
 let productElement = document.getElementById("product-name");
 let remainingCharsElement = document.getElementById("remaining-chars");
 
+function updateCharacters(event) {
+    let enteredText = event.target.value;
+    let enteredTextLength = enteredText.length;
+}
+
+productElement.addEventListener("input", updateCharacters);

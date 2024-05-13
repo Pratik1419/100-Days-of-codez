24302abc73915b63cdf732ffe51dab6f07f0772f
Pratik Mishra <pratikmishra141@gmail.com>
2024-05-13 14:58:57 +0530

    Create loops.js

---
+++ js-by-Maximilian/loops.js
@@ -0,0 +1,21 @@
+const users = ["Max", "Roy", "Joy"];
+
+for (const user of users) {
+    console.log(user);
+}
+
+const loggedId = {
+    name: "Roy",
+    age: 22,
+    isAdmin: true
+};
+
+for (const key in loggedId ) {
+    console.log(loggedId[key]);
+}
+
+let isFinished = false;
+
+while (!isFinished) {
+    confirm("Do you want to quit")   //By using confirm it will gives us the result with yes/no value.
+}
